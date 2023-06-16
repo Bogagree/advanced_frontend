93b7ca3a142aa06f5c9ac15d9287933f13f5749c
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-06-16 17:25:25 +0300

    feat: add react-router-dom, code-spliting with react-lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from './components/Counter';
 import './index.scss'
+import {AboutPageAsync} from './components/pages/AboutPage/AboutPage.async';
+import {MainPageAsync} from './components/pages/MainPage/MainPage.async';
 
 export const App = () => {
     return (
         <div className="app">
-            Feature slice design
+            <div>Feature slice design</div>
+
+            <Link to={'/'} style={{margin: 10}}>Главная страница</Link>
+            <Link to={'/about'} style={{margin: 10}}>О проекте</Link>
+
+            <Suspense fallback={<div style={{color: 'red'}}>Loading ...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={''} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+
             <Counter/>
         </div>
     );
