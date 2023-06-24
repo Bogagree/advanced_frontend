606fdc673944186f993c767b39e4bbabe8da611e
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-06-24 17:06:39 +0300

    feat: add FSD architecture, absolute import for tsconfig.ts, webpack.config.ts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
-import {Counter} from './components/Counter';
 import './styles/index.scss'
-import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
-import {MainPageAsync} from './pages/MainPage/MainPage.async';
-import {useTheme} from './theme/useTheme';
-import {classNames} from './helpers/classNames/classNames';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProviders';
+import {AboutPage} from 'pages/AboutPage';
+import {MainPage} from 'pages/MainPage';
 
 
 export const App = () => {
@@ -13,7 +12,6 @@ export const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        // <div className={`app ${theme}`}>
         <div className={classNames('app', {}, [theme])}>
             <div>Feature slice design</div>
 
@@ -24,12 +22,11 @@ export const App = () => {
 
             <Suspense fallback={<div style={{color: 'red'}}>Loading ...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={''} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={''} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
-            <Counter/>
         </div>
     );
 };
