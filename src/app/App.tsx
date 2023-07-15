dd60c31f86d217f5bd7040af50e0a1f84a8b46ef
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-07-15 14:50:31 +0300

    feat: add Navbar, AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {classNames} from 'shared/lib/classNames/classNames';
 import {useTheme} from 'app/providers/ThemeProviders';
 import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
 
 
 export const App = () => {
@@ -13,13 +13,9 @@ export const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <div>Feature slice design</div>
-
-            <button onClick={toggleTheme}>Toggle theme</button>
-
-            <Link to={'/'} style={{margin: 10}}>Главная страница</Link>
-            <Link to={'/about'} style={{margin: 10}}>О проекте</Link>
-
+            <Navbar/>
             <AppRouter/>
+            <button onClick={toggleTheme}>Toggle theme</button>
         </div>
     );
 };
