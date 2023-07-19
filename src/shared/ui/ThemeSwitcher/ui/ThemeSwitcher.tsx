c09708b19b716edfc149584c9da1a91a5b2e025f
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-07-19 17:49:55 +0300

    feat: add custom button

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {useTheme} from 'app/providers/ThemeProviders';
+import {Theme, useTheme} from 'app/providers/ThemeProviders';
 import {classNames} from 'shared/lib/classNames/classNames';
 import cls from './ThemeSwitcher.module.scss'
 import DarkIcon from 'shared/assets/icons/dark-theme.svg'
 import LightIcon from 'shared/assets/icons/light-theme.svg'
+import {Button, ThemeButton} from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
     className?: string
@@ -11,10 +12,12 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
 
-    const {toggleTheme} = useTheme()
+    const {toggleTheme, theme} = useTheme()
     return (
-        <button onClick={toggleTheme} className={classNames(cls.ThemeSwitcher, {}, [className])}>
-            <DarkIcon style={{width: '50px', height: '50px'}}/>
-        </button>
+        <Button onClick={toggleTheme} className={classNames(cls.ThemeSwitcher, {}, [className, cls.icon])}
+                theme={ThemeButton.CLEAR}>
+            {theme === Theme.DARK ? <DarkIcon/> :
+                <LightIcon/>}
+        </Button>
     );
 };
