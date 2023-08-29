d6e1ee2436b06a797f04c9dc9e55331d4228e876
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-08-29 19:05:48 +0300

    feat: add ignore in eslint for test files with i18next

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
         indent: [2, 4],
         'react/jsx-filename-extension': [
             2,
-            { extensions: ['.js', '.jsx', '.tsx'] },
+            {extensions: ['.js', '.jsx', '.tsx']},
         ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
@@ -46,12 +46,20 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttributes:
-                    ['data-testid']
-                 }],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+                ignoreAttributes: ['data-testid']
+            }
+        ],
+        'max-len': ['error', {ignoreComments: true, code: 100}],
     },
     globals: {
         __IS_DEV__: true,
     },
+    overrides: [
+        {
+            files: ['**/src/**/*.test.[ts,tsx}'],
+            rules: {
+                'i18next/no-literal-string': 'off',
+            }
+        }
+    ]
 };
