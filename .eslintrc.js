63d3d7ddfd8f156e81ea116cb7b30dd526a5fced
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-08-29 14:07:28 +0300

    feat: add BundleAnalyzerPlugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'react/prop-types': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
-        'max-len': ['error', { ignoreComments: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
         __IS_DEV__: true,
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { App } from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProviders';
-
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
