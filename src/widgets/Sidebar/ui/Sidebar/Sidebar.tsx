5cb19e7994649bc1aa18adf1d7137d18a3d6caf6
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-08-31 13:20:34 +0300

    fix: eslint conflicts

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,8 +2,8 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useState } from 'react';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
-import { Button } from '../../../../shared/ui/Button/Button';
 
 interface SidebarProps {
     className?: string
