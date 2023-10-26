10dc281c6ead152150f5c4c2c38fece6f10324b2
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-10-26 20:47:29 +0300

    add npm install eslint-plugin-storybook@latest

--- src/shared/config/storybook/RouteDecorator/RouteDecorator.tsx
+++
@@ -1,8 +0,0 @@
-import {Story} from '@storybook/react';
-import {BrowserRouter} from 'react-router-dom';
-
-export const RouteDecorator = (story: () => Story) => (
-    <BrowserRouter>
-        {story()}
-    </BrowserRouter>
-);
