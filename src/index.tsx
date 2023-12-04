589fdd3d5221779302e95c02db730299c0ae8d47
Dmitry Bogatyrev <Bogagree@gmail.com>
2023-12-04 21:40:43 +0300

    add reduxToolkit, StoreProvider

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,17 @@ import { ThemeProvider } from 'app/providers/ThemeProviders';
 import './shared/config/i18n/i18n';
 import 'app/styles/index.scss';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
