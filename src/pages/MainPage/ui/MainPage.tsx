313f00a5d2aa87546fa782f14aae1d40844fa5e9
Dmitry Bogatyrev <Bogagree@gmail.com>
2024-01-17 10:27:59 +0300

    approve screenshots

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,22 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
-import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
 
-    const [value, setValue] = useState('');
-
-    const onChange = (val: string) => {
-        setValue(val);
-    };
     return (
         <div>
             <h1>{t('mainPageTitle')}</h1>
             <BugButton />
-            <Counter />
         </div>
     );
 };
